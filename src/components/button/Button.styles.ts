67c647df80631b9aca737236e125013f9a696be2
Jonathas Lima <alexandrejonathas@gmail.com>
2021-07-13 10:18:40 -0300

    Finalizando o componente botão com polished

--- src/components/button/Button.styles.ts
+++ src/components/button/Button.styles.ts
@@ -1,4 +1,11 @@
 import styled from "styled-components"
+import { transparentize } from "polished"
+
+const COLORS = {
+    primary: '#0099FF',
+    danger: '#F84735',
+    foreground: '#274060'
+}
 
 const THEME = {
     primary: {
@@ -6,21 +13,33 @@ const THEME = {
         color: '#FFFFFF',
         onHover: `
             box-shadow: 0 3px 6px rgba(0,0,0,0.1);
-        `
+        `,
+        disabled: {
+            background: transparentize(0.44, COLORS.primary),
+            color: '#FFFFFF'
+        }
     },
     danger: {
         background: '#F84735',
         color: '#FFFFFF',
         onHover: `
             box-shadow: 0 3px 6px rgba(0,0,0,0.1);
-        `
+        `,
+        disabled: {
+            background: transparentize(0.75, COLORS.danger),
+            color: COLORS.danger
+        }
     },
     text: {
         background: 'transparent',
         color: '#274060',
         onHover: `
             border-color: #274060;
-        `
+        `,
+        disabled: {
+            background: transparentize(0.44, '#508AC9'),
+            color: COLORS.foreground
+        }
     }
 }
 
@@ -34,7 +53,15 @@ export const Wrapper = styled.button<{
     
     &:hover,
     &:focus {
-        ${ props => THEME[props.variant].onHover }    
+        ${ props => THEME[props.variant].onHover };    
+    }
+
+    &:disabled {
+        background: ${props => THEME[props.variant].disabled.background };
+        color: ${props => THEME[props.variant].disabled.color };
+
+        pointer-events: none;
+        border-color: transparent;
     }
 
 `
