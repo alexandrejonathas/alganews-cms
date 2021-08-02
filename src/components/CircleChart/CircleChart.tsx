d0bb757016013feb1d50cf24942150e2acd9ebe1
Jonathas Lima <alexandrejonatha@gmail.com>
2021-08-02 09:43:35 -0300

    Documentando o CircleChart

--- src/components/CircleChart/CircleChart.tsx
+++ src/components/CircleChart/CircleChart.tsx
@@ -1,6 +1,12 @@
 import * as C from './CircleChart.styles'
 
-export interface CircleChartProps {}
+export interface CircleChartProps {
+    size: number,
+    progress: number,
+    caption?: string,
+    theme?: 'default' | 'primary',
+    strokeWidht?: number
+}
 
 export default function CircleChart (props : CircleChartProps) {
     return <C.Wrapper>
