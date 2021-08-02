76cd5cb9322427d44c5f339c85dae16d8df2d9b3
Jonathas Lima <alexandrejonatha@gmail.com>
2021-08-02 09:32:55 -0300

    Criação com componente CircleChart

---
+++ src/components/CircleChart/CircleChart.tsx
@@ -0,0 +1,9 @@
+import * as C from './CircleChart.styles'
+
+export interface CircleChartProps {}
+
+export default function CircleChart (props : CircleChartProps) {
+    return <C.Wrapper>
+        todo: circle chart
+    </C.Wrapper>
+}
---
+++ src/stories/CircleChart.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import CircleChart, { CircleChartProps } from '../components/CircleChart';
+
+export default {
+  title: 'Example/CircleChart',
+  component: CircleChart,
+} as Meta;
+
+const Template: Story<CircleChartProps> = (args) => <CircleChart {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {}
