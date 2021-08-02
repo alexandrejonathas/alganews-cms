e23fa713df002c50e585222990877350b47827f2
Jonathas Lima <alexandrejonatha@gmail.com>
2021-08-02 14:14:09 -0300

    Criação do component ImageUpload

---
+++ src/components/ImageUpload/ImageUpload.styles.ts
@@ -0,0 +1,56 @@
+import styled from "styled-components"
+import { Wrapper as Button } from '../Button/Button.styles'
+
+
+export const Wrapper = styled.div`
+`
+
+export const Label = styled.label`
+    background: #0099FF;
+    color: #FFFFFF;
+
+    padding: 24px;
+    cursor: pointer;
+
+    display: flex;
+    align-items: center;
+    grid-gap: 12px;
+`
+
+export const Input = styled.input`
+    display: none;
+`
+
+
+export const ImagePreview = styled.div<{ preview: string }>`
+    height: 100%;
+    background-image: url(${ props => props.preview });
+    background-position: center;
+    background-size: cover;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+`
+
+export const ImagePreviewWrapper = styled.div`
+    background-color: #274060;
+    height: 240px;
+
+    ${Button} {
+        display: none;
+    }
+
+    &:hover {
+
+        ${ImagePreview} {
+            opacity: 0.7;
+        }
+
+        ${Button} {
+            display: block;         
+        }
+    }
+
+`
---
+++ src/stories/ImageUpload.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react';
+
+import ImageUpload, { ImageUploadProps } from '../components/ImageUpload';
+
+export default {
+  title: 'Example/ImageUpload',
+  component: ImageUpload,
+} as Meta;
+
+const Template: Story<ImageUploadProps> = (args) => <ImageUpload {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: 'Thumbnail do post'
+}
