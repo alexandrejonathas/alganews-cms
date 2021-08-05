778ce806150e3ebedf5c9793bcb643266bda1b3b
Jonathas Lima <alexandrejonatha@gmail.com>
2021-08-05 13:54:25 -0300

    Criação do component MarkdownEditor

---
+++ src/components/MarkdownEditor/MarkdownEditor.tsx
@@ -0,0 +1,14 @@
+import MarkdownIt from 'markdown-it'
+import MdEditor, { Plugins } from 'react-markdown-editor-lite'
+import 'react-markdown-editor-lite/lib/index.css'
+
+//Desabilita o underline do control
+MdEditor.unuse(Plugins.FontUnderline)
+
+const parser = new MarkdownIt()
+
+export interface MarkdownEditorProps {}
+
+export default function MarkdownEditor () {
+    return <MdEditor style={{ height: 300 }} renderHTML={ text => parser.render(text) } />
+}
