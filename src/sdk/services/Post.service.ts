e7b77d6b8a2483a1bc333a094363d866a7253855
Jonathas Lima <alexandrejonatha@gmail.com>
2021-08-25 18:11:31 -0300

    Criando metodo createPoste no serviço

--- src/sdk/services/Post.service.ts
+++ src/sdk/services/Post.service.ts
@@ -4,17 +4,23 @@ import { Post } from '../@types'
 
 class PostService extends Service {
 
-    static getAllPosts() {
+    static getAllPosts () {
         return this.Http
             .get<Post.Paginated>('/posts')
             .then(this.getData)
     }
 
-    static getExistingPost(id: number) {
+    static getExistingPost (id: number) {
         return this.Http
             .get<Post.Paginated>(`/posts/${id}`)
             .then(this.getData)
-    }    
+    }
+    
+    static createPost (post: Post.Input) {
+        return this.Http
+            .post<Post.Detailed>(`/posts`, post)
+            .then(this.getData)
+    }      
 
 }
 
